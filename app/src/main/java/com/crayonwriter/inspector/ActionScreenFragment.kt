b966cc39b62e171ecd1b87b73c812bb3007f9d3f
Theda Rogers <crayonwriter@gmail.com>
2020-07-18 00:20:06 -0400

    Create ActionScreenFragment.kt and its onCreateView method

---
+++ app/src/main/java/com/crayonwriter/inspector/ActionScreenFragment.kt
@@ -0,0 +1,19 @@
+package com.crayonwriter.inspector
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ActionScreenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+        ): View? {
+        val view = inflater.inflate(R.layout.fragment_action_screen, container, false)
+        return view
+    }
+}
